54cdc9dd8155767f2c8aa6e10ec41c48e33399dc
Abhishek Raman <beastonlineg@gmail.com>
2023-06-25 08:18:38 +0530

    fixed searchengine.tsx and search.ts has dummy dat

--- src/app/searchengine.tsx
+++ src/app/searchengine.tsx
@@ -1,34 +1,38 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+interface SearchResult {
+  id: number;
+  title: string;
+}
+
 const SearchEngine = () => {
-    const [searchQuery, setSearchQuery] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
-  
-    const handleSearch = async () => {
-      try {
-        const response = await axios.get(`/api/search?query=${searchQuery}`);
-        setSearchResults(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-  
-    return (
-      <div>
-        <input
-          type="search"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          placeholder="Enter your search query"
-        />
-        <button onClick={handleSearch}>Search</button>
-        {searchResults.map((result) => (
-          <div key={result.id}>{result.title}</div>
-        ))}
-      </div>
-    );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`/api/search?query=${searchQuery}`);
+      setSearchResults(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
-  
-  export default SearchEngine;
-  
+
+  return (
+    <div>
+      <input
+        type="search"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Enter your search query"
+      />
+      <button onClick={handleSearch}>Search</button>
+      {searchResults.map((result) => (
+        <div key={result.id}>{result.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default SearchEngine;
